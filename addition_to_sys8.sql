86fc41f5aea579b810545755ad75ab62844cae06
wubx <wubx@zhishutang.com>
2019-07-10 15:54:59 +0800

    MySQL 8.0 mgr support

--- addition_to_sys8.sql
+++ addition_to_sys8.sql
@@ -1,5 +1,4 @@
 USE sys;
-
 DELIMITER $$
 
 CREATE FUNCTION IFZERO(a INT, b INT)
@@ -55,6 +54,7 @@ WHERE Channel_name = 'group_replication_applier' ), (SELECT
 @@global.GTID_EXECUTED) )));
 END$$
 
+
 CREATE FUNCTION gr_member_in_primary_partition()
 RETURNS VARCHAR(3)
 DETERMINISTIC
@@ -63,14 +63,35 @@ BEGIN
 performance_schema.replication_group_members WHERE MEMBER_STATE != 'ONLINE') >=
 ((SELECT COUNT(*) FROM performance_schema.replication_group_members)/2) = 0),
 'YES', 'NO' ) FROM performance_schema.replication_group_members JOIN
-performance_schema.replication_group_member_stats USING(member_id) where member_id=@@server_uuid);
+performance_schema.replication_group_member_stats USING(member_id)
+          where performance_schema.replication_group_members.member_host=@@hostname);
 END$$
 
-CREATE VIEW gr_member_routing_candidate_status AS SELECT
-sys.gr_member_in_primary_partition() as viable_candidate,
-IF( (SELECT (SELECT GROUP_CONCAT(variable_value) FROM
-performance_schema.global_variables WHERE variable_name IN ('read_only',
-'super_read_only')) != 'OFF,OFF'), 'YES', 'NO') as read_only,
-sys.gr_applier_queue_length() as transactions_behind, Count_Transactions_in_queue as 'transactions_to_cert' from performance_schema.replication_group_member_stats;$$
+CREATE VIEW gr_member_routing_candidate_status AS
+    SELECT 
+        sys.gr_member_in_primary_partition() AS viable_candidate,
+        IF((SELECT 
+                    (SELECT 
+                                GROUP_CONCAT(variable_value)
+                            FROM
+                                performance_schema.global_variables
+                            WHERE
+                                variable_name IN ('read_only' , 'super_read_only')) != 'OFF,OFF'
+                ),
+            'YES',
+            'NO') AS read_only,
+        sys.gr_applier_queue_length() AS transactions_behind,
+        Count_Transactions_in_queue AS 'transactions_to_cert'
+    FROM
+        performance_schema.replication_group_member_stats a
+            JOIN
+        performance_schema.replication_group_members b ON a.member_id = b.member_id
+    WHERE
+        b.member_host IN (SELECT 
+                variable_value
+            FROM
+                performance_schema.global_variables
+            WHERE
+                variable_name = 'hostname')$$
 
 DELIMITER ;
